12c91eaecaef10e2d16b4c3acb50af719b0e662a
yonela mphini <122893486+yoniem@users.noreply.github.com>
2024-08-21 21:12:54 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,35 +1,12 @@
-{
-  "name": "recipe-finder-app",
-  "version": "1.0.0",
-  "private": true,
-  "dependencies": {
-    "react": "^18.2.0",
-    "react-dom": "^18.2.0",
-    "react-router-dom": "^6.26.0",
-    "bootstrap": "^5.3.3"
-  },
-  "scripts": {
-    "start": "react-scripts start",
-    "build": "react-scripts build",
-    "test": "react-scripts test",
-    "eject": "react-scripts eject"
-  },
-  "eslintConfig": {
-    "extends": [
-      "react-app",
-      "react-app/jest"
-    ]
-  },
-  "browserslist": {
-    "production": [
-      ">0.2%",
-      "not dead",
-      "not op_mini all"
-    ],
-    "development": [
-      "last 1 chrome version",
-      "last 1 firefox version",
-      "last 1 safari version"
-    ]
-  }
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importing Bootstrap CSS
+import './App.css'; // Importing custom styles
+import App from './components/App'; // Main App component
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root') // Render the App in the root element
+);
